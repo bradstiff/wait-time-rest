7078d09e03200b83440e6887c0cda42300aeb66d
Brad <brad@xovation.com>
2018-09-17 14:18:37 -0600

    Refactor to not depend on react-dimensions

--- ClientApp/src/waittime/TimeSlider.js
+++ ClientApp/src/waittime/TimeSlider.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import Slider from 'rc-slider';
 import moment from 'moment';
 import { compose } from 'redux';
-import withDimensions from 'react-dimensions';
+import withWidth from '@material-ui/core/withWidth';
 
 import 'rc-slider/assets/index.css';
 
 import { WaitTimeDateShape } from './types';
 
-const handleStyle = {
+const dragHandleStyle = {
     borderColor: '#D44126',
 };
 const containerStyle = {
@@ -21,10 +21,26 @@ class TimeSlider extends React.Component {
     static propTypes = {
         waitTimeDate: WaitTimeDateShape,
         onSelectTimePeriod: PropTypes.func,
-        containerWidth: PropTypes.number,
     };
 
-    getSliderMarks(timePeriods) {
+    get hasWaitTimeDate() {
+        const { selectedTimestamp, timePeriods } = this.props.waitTimeDate || {};
+        return !!(selectedTimestamp && timePeriods && timePeriods.length);
+    }
+
+    setContainerRef = ref => {
+        this.container = ref;
+    }
+
+    componentDidMount() {
+        if (this.hasWaitTimeDate) {
+            //received data before DOM; re-render as marks depend on container dimensions
+            //should never happen
+            this.forceUpdate();
+        }
+    }
+
+    getSliderValues() {
         /* ********************************************************************************
          * Calculate the marks to display on the slider. Each mark corresponds to a time of 
          * day, and is labeled according to culture.  We display as many marks as will fit,
@@ -33,14 +49,19 @@ class TimeSlider extends React.Component {
          * period (e.g., every 15 minutes). For small containers or large # of time periods,
          * we display a mark for every x time periods, e.g., every 30 minutes, 45 minutes, etc.
          * *******************************************************************************/
-        const { containerWidth } = this.props;
-        if (!containerWidth) {
-            return null;
+        if (!this.container ||
+            !this.container.clientWidth ||
+            !this.hasWaitTimeDate) {
+            return {};
         }
 
+        const containerWidth = this.container.clientWidth;
+        const { waitTimeDate: { timePeriods, selectedTimestamp }} = this.props;
+
+        const secondsPerPeriod = 15 * 60; //each time period is 15 minutes
         const marksToDisplay = containerWidth / 50; //allow 50px per mark
         const periodsPerMark = Math.ceil(timePeriods.length / marksToDisplay);
-        const secondsPerMark = 15 * 60 * periodsPerMark;  //each time period is 15 minutes
+        const secondsPerMark = secondsPerPeriod * periodsPerMark;  
         const startSeconds = timePeriods[0].timestamp;
 
         const timePeriodsToDisplay = periodsPerMark > 1
@@ -49,40 +70,43 @@ class TimeSlider extends React.Component {
             //display a mark for every time period, e.g., every 15 minutes
             : timePeriods;
 
-        return timePeriodsToDisplay.reduce((marks, timePeriod) => {
+        const marks = timePeriodsToDisplay.reduce((marks, timePeriod) => {
             marks[timePeriod.timestamp.toString()] = moment.unix(timePeriod.timestamp).utc().format('LT');
             return marks;
         }, {});
+
+        return {
+            value: selectedTimestamp,
+            marks,
+            min: startSeconds,
+            max: timePeriodsToDisplay[timePeriodsToDisplay.length - 1].timestamp,
+            step: secondsPerPeriod,
+        }
     }
 
     render() {
-        const { selectedTimestamp, timePeriods } = this.props.waitTimeDate || {};
-        if (!selectedTimestamp || !timePeriods || !timePeriods.length) {
-            return null;
-        }
-
-        const marks = this.getSliderMarks(timePeriods);
-        if (!marks) {
-            return null;
-        }
-
+        const { value, marks, min, max, step } = this.getSliderValues();
+        const { onSelectTimePeriod } = this.props;
         return (
-            <div style={containerStyle}>
-                <Slider
-                    marks={marks}
-                    min={timePeriods[0].timestamp}
-                    max={timePeriods[timePeriods.length - 1].timestamp}
-                    step={15 * 60}
-                    included={false}
-                    value={selectedTimestamp}
-                    onChange={this.props.onSelectTimePeriod}
-                    handleStyle={handleStyle}
-                />
+            <div style={containerStyle} ref={this.setContainerRef}>
+                {
+                    this.hasWaitTimeDate
+                        ? <Slider
+                            value={value}
+                            marks={marks}
+                            min={min}
+                            max={max}
+                            step={step}
+                            included={false}
+                            onChange={onSelectTimePeriod}
+                            handleStyle={dragHandleStyle} />
+                        : null
+                }
             </div>
         );
     }
 }
 
 export default compose(
-    withDimensions({ debounce: 166 }), //10 frames at 60Hz
+    withWidth(), //updates on window resize
 )(TimeSlider);
