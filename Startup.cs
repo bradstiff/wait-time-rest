bbb22fcbac8a96da8396d85c2da5c626c75e45bd
Brad <brad@xovation.com>
2018-10-01 12:07:23 -0600

    Use Rollbar in production

--- Startup.cs
+++ Startup.cs
@@ -1,46 +1,58 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 
+using Rollbar.AspNetCore;
 using WaitTime.Entities;
 
 namespace WaitTime
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
+            this.Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Rollbar needs HttpContext
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            if (!this.Environment.IsDevelopment())
+            {
+                //Use Rollbar for logging if not development
+                services.AddRollbarLogger();
+            }
+
+            //explicitly inject WaitTimeContext into controller instances
             services.AddDbContext<WaitTimeContext>(options => options.UseSqlServer(Configuration["DbConnection"]));
+
             services.AddMvc();
 
-            // In production, the React files will be served from this directory
+            // React app production build static file location
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // Add middleware to pipeline
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (!this.Environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+                //Use Rollbar to log all unhandled exceptions
+                app.UseRollbarMiddleware();
+                //app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
@@ -57,7 +69,7 @@ namespace WaitTime
             {
                 spa.Options.SourcePath = "ClientApp";
 
-                if (env.IsDevelopment())
+                if (this.Environment.IsDevelopment())
                 {
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
