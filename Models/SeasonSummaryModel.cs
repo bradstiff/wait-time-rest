df9bc752e5c32719f87b5f8d798af39449c3a26f
Brad Stiff <brad@xovation.com>
2020-05-16 11:14:17 -0600

    Naming

--- Models/SeasonSummaryModel.cs
+++ Models/SeasonSummaryModel.cs
@@ -10,12 +10,12 @@ namespace WaitTime.Models
         public int? Year { get; set; }
         public string Name { get; set; }
         public string FavoriteResort { get; set; }
-        public int FavoriteResortDays { get; set; }
+        public int FavoriteResortDaysCount { get; set; }
         public string FavoriteLift { get; set; }
-        public int FavoriteLiftUplifts { get; set; }
-        public int Days { get; set; }
-        public int Resorts { get; set; }
-        public int Runs { get; set; }
+        public int FavoriteLiftUpliftsCount { get; set; }
+        public int DaysCount { get; set; }
+        public int ResortsCount { get; set; }
+        public int RunsCount { get; set; }
         public int SkiTimeSeconds { get; set; }
         public float SkiDistanceMeters { get; set; }
         public int SkiVerticalMeters { get; set; }
